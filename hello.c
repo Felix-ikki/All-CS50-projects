6178db04bbdf9e8d29a9b7bdf6e635cd6b85ee1c
Felix-ikki <90804919+Felix-ikki@users.noreply.github.com>
2021-09-23 18:04:40 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // what´s your name?
+    string name = get_string("What is your name?\n");
+    
+    // say hello + name
+    printf("hello, %s\n", name
+}
